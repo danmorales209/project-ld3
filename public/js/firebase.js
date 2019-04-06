875422d76ed441d4f79f140f102db7b187f176b6
Lindca <45475418+Lindca@users.noreply.github.com>
2019-04-06 15:05:03 -0700

    authentication

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,92 +1,98 @@
-// $(document).ready(function () {
-//     displayLogin();
+function displayLogin() {
+    $("#loginDiv").show();
+    $("#registerDiv").hide();
+};
 
-//     // Function to run when user clicks Register button in navbar
-//     $("#navRegister").click(displayRegister);
+function displayRegister() {
+    $("#registerDiv").show();
+    $("#loginDiv").hide();
+};
 
-//     function displayRegister() {
-//         $("#registerDiv").show();
-//         $("#loginDiv").hide();
-//     }
+function userLogOut() {
+    firebase.auth().signOut();
+    $("#loginDiv").show();
+    $("#registerDiv").hide();
+};
 
-//     // Function to run when user clicks Login button in navbar
-//     $("#navLogin").click(displayLogin);
+$(document).ready(function () {
+    displayLogin();
 
-//     function displayLogin() {
-//         console.log("login clicked");
-//         $("#loginDiv").show();
-//         $("#registerDiv").hide();
-//     }
+    // Function to run when user clicks Register button in navbar
+    $("#navRegister").click(displayRegister);
 
-//     // Function to run when user clicks Logout button in navbar
-//     $("#navLogout").click(userLogOut);
+    // Function to run when user clicks Login button in navbar
+    $("#navLogin").click(displayLogin);
 
-//     function userLogOut() {
-//         console.log("logout clicked");
-//         // firebase.auth().signOut();
-//         $("#loginDiv").hide();
-//         $("#registerDiv").hide();
-//     };
+    // Function to run when user clicks Logout button in navbar
+    $("#navLogout").on("click", function (event) {
+        console.log("logout clicked");
+        event.preventDefault();
+        userLogOut();
+    });
 
-//     $("#showLogin").click(displayLogin);
 
-//     $("#showRegister").click(displayRegister);
+    $("#showLogin").click(displayLogin);
 
-//     // Firebase setup
-//     // Initialize Firebase
-//     var config = {
-//         apiKey: "AIzaSyC1BNApFh7kXWjeklAQsLoPaAQGbOw5QKI",
-//         authDomain: "my-grade-book.firebaseapp.com",
-//         databaseURL: "https://my-grade-book.firebaseio.com",
-//         projectId: "my-grade-book",
-//         storageBucket: "my-grade-book.appspot.com",
-//         messagingSenderId: "928221026384"
-//     };
-//     firebase.initializeApp(config);
+    $("#showRegister").click(displayRegister);
 
-//     // Function to run based on if user is logged in or not
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         if (user) {
-//             console.log(user);
-//             // User is signed in
-//             // display landing page
-//         } else {
-//             // No user is signed in.
-//             // userLogOut();
-//             // display sorry you are not recognized by our database as a student
-//         }
-//     });
+    // Firebase setup
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyC1BNApFh7kXWjeklAQsLoPaAQGbOw5QKI",
+        authDomain: "my-grade-book.firebaseapp.com",
+        databaseURL: "https://my-grade-book.firebaseio.com",
+        projectId: "my-grade-book",
+        storageBucket: "my-grade-book.appspot.com",
+        messagingSenderId: "928221026384"
+    };
+    firebase.initializeApp(config);
 
-//     // Function to register a new user
-//     $("#register").on("submit", function (event) {
-//         event.preventDefault();
+    var database = firebase.database();
 
-//         var email = $("#register-email").val();
-//         var password = $("#register-password").val();
-//         console.log(email);
-//         console.log(password);
-//         firebase.auth().createUserWithEmailAndPassword(email, password)
-//             .then(function (user) {
-//                 console.log(user);
-//             })
-//             .catch(function (err) {
-//                 console.log(err);
-//             })
-//     });
+    // Function to run based on if user is logged in or not
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            console.log("user is signed in!");
+        } else {
+            console.log("no user signed in!");
+            // No user is signed in.
+            userLogOut();
+        }
+    });
 
-//     // Function to login existing user
-//     $("#login").on("submit", function (event) {
-//         event.preventDefault();
+    // Function to register a new user
+    $("#register").on("submit", function (event) {
+        event.preventDefault();
+        console.log("register was clicked!")
+        var email = $("#register-email").val();
+        var password = $("#register-password").val();
+        console.log(email);
+        console.log(password);
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function (user) {
+                console.log(user);
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+    });
 
-//         var email = $(".login .email").val();
-//         var password = $(".login .password").val();
+    // Function to login existing user
+    $("#login").on("submit", function (event) {
+        event.preventDefault();
 
-//         firebase.auth().signInWithEmailAndPassword(email, password)
-//             .then(function (user) {
-//                 console.log(user);
-//             })
-//             .catch(function (err) {
-//                 console.log(err);
-//             });
-//     });
-// })
+        var email = $("#login-email").val();
+        var password = $("#login-password").val();
+        console.log("logged in");
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(function (user) {
+                console.log("hello")
+                console.log(firebase.auth().currentUser);
+                // window.location.replace("./teachLanding.html");
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    });
+})
