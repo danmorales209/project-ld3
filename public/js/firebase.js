a07f0c7ea98b6445bd7110a7a22cbcfdd3c0892a
Lindca <45475418+Lindca@users.noreply.github.com>
2019-04-06 13:24:58 -0700

    authnotworking

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,94 +1,92 @@
-$(document).ready(function () {
-    displayLogin();
+// $(document).ready(function () {
+//     displayLogin();
 
-// Function to run when user clicks Register button in navbar
-$("#navRegister").click(displayRegister);
+//     // Function to run when user clicks Register button in navbar
+//     $("#navRegister").click(displayRegister);
 
-    function displayRegister() {
-        $("#registerDiv").show();
-$("#loginDiv").hide();
-}
+//     function displayRegister() {
+//         $("#registerDiv").show();
+//         $("#loginDiv").hide();
+//     }
 
-// Function to run when user clicks Login button in navbar
-$("#navLogin").click(displayLogin);
+//     // Function to run when user clicks Login button in navbar
+//     $("#navLogin").click(displayLogin);
 
-function displayLogin() {
-    console.log("login clicked");
-    $("#loginDiv").show();
-    $("#registerDiv").hide();
-}
+//     function displayLogin() {
+//         console.log("login clicked");
+//         $("#loginDiv").show();
+//         $("#registerDiv").hide();
+//     }
 
-// Function to run when user clicks Logout button in navbar
-$("#navLogout").click(userLogOut);
+//     // Function to run when user clicks Logout button in navbar
+//     $("#navLogout").click(userLogOut);
 
-function userLogOut() {
-    console.log("logout clicked");
-    // firebase.auth().signOut();
-    $("#loginDiv").hide();
-    $("#registerDiv").hide();
-};
+//     function userLogOut() {
+//         console.log("logout clicked");
+//         // firebase.auth().signOut();
+//         $("#loginDiv").hide();
+//         $("#registerDiv").hide();
+//     };
 
-$("#showLogin").click(displayLogin);
+//     $("#showLogin").click(displayLogin);
 
-$("#showRegister").click(displayRegister);
+//     $("#showRegister").click(displayRegister);
 
-// Firebase setup
-// Initialize Firebase
-firebase.initializeApp(config);
+//     // Firebase setup
+//     // Initialize Firebase
+//     var config = {
+//         apiKey: "AIzaSyC1BNApFh7kXWjeklAQsLoPaAQGbOw5QKI",
+//         authDomain: "my-grade-book.firebaseapp.com",
+//         databaseURL: "https://my-grade-book.firebaseio.com",
+//         projectId: "my-grade-book",
+//         storageBucket: "my-grade-book.appspot.com",
+//         messagingSenderId: "928221026384"
+//     };
+//     firebase.initializeApp(config);
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCxWT735EGIcbdNLZyxsWYns14mJ7UvOtg",
-    authDomain: "mygradebook-355d0.firebaseapp.com",
-    databaseURL: "https://mygradebook-355d0.firebaseio.com",
-    projectId: "mygradebook-355d0",
-    storageBucket: "mygradebook-355d0.appspot.com",
-    messagingSenderId: "619478408217"
-  };
+//     // Function to run based on if user is logged in or not
+//     firebase.auth().onAuthStateChanged(function (user) {
+//         if (user) {
+//             console.log(user);
+//             // User is signed in
+//             // display landing page
+//         } else {
+//             // No user is signed in.
+//             // userLogOut();
+//             // display sorry you are not recognized by our database as a student
+//         }
+//     });
 
-// Function to run based on if user is logged in or not
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        console.log(user);
-        // User is signed in
-        // display landing page
-    } else {
-        // No user is signed in.
-        userLogOut();
-        // display sorry you are not recognized by our database as a student
-    }
-});
+//     // Function to register a new user
+//     $("#register").on("submit", function (event) {
+//         event.preventDefault();
 
-// Function to register a new user
-$("#register").on("submit", function (event) {
-    event.preventDefault();
+//         var email = $("#register-email").val();
+//         var password = $("#register-password").val();
+//         console.log(email);
+//         console.log(password);
+//         firebase.auth().createUserWithEmailAndPassword(email, password)
+//             .then(function (user) {
+//                 console.log(user);
+//             })
+//             .catch(function (err) {
+//                 console.log(err);
+//             })
+//     });
 
-    var email = $("#register-email").val();
-    var password = $("#register-password").val();
-    console.log(email);
-    console.log(password);
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(function (user) {
-            console.log(user);
-        })
-        .catch(function (err) {
-            console.log(err);
-        })
-});
+//     // Function to login existing user
+//     $("#login").on("submit", function (event) {
+//         event.preventDefault();
 
-// Function to login existing user
-$("#login").on("submit", function (event) {
-    event.preventDefault();
+//         var email = $(".login .email").val();
+//         var password = $(".login .password").val();
 
-    var email = $(".login .email").val();
-    var password = $(".login .password").val();
-
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(function (user) {
-            console.log(user);
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-});
-})
+//         firebase.auth().signInWithEmailAndPassword(email, password)
+//             .then(function (user) {
+//                 console.log(user);
+//             })
+//             .catch(function (err) {
+//                 console.log(err);
+//             });
+//     });
+// })
