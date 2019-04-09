37f59ae216fb37c8effdfa3e6e2e16b1765df119
Dan <danmorales209@gmail.com>
2019-04-08 21:12:53 -0700

    fix some things

--- public/js/teacherLanding.js
+++ public/js/teacherLanding.js
@@ -2,8 +2,24 @@ $(document).ready(function () {
   let url = "/api/students";
   $.get(url).then(function (response) {
     let students = response;
+    let overallGrade = [];
 
     students.forEach(student => {
+
+      if (student.Grades.length > 0) {
+        overallGrade.push(student.Grades.reduce((sum, grade) => {
+          return sum + (grade.gradeValue / grade.Assignment.maxPoints);
+        }, 0));
+      }
+      else {
+        overallGrade.push(0);
+      }
+      
+
+
+      console.log(studentGrade);
+
+
       let idParagraph = $("<p>")
         .text(student.id)
         .addClass("col-3 display-5")
@@ -14,6 +30,8 @@ $(document).ready(function () {
         .addClass("col-9 display-5")
         .appendTo($("#display-students"));
     });
+
+    console.log(overallGrade);
 
     var ctx = document.getElementById("myChart").getContext('2d');
     var chart = new Chart(ctx, {
@@ -33,49 +51,6 @@ $(document).ready(function () {
       options: {}
     });
 
-
   });
-
-  // DUMMY DATA //
-
-  /* let students = [
-    { name: "Al", grade: .9 },
-    { name: "Bill", grade: .2 },
-    { name: "Casey", grade: .7 },
-    { name: "Danielle", grade: .9 },
-    { name: "Ele", grade: .2 },
-    { name: "Fanny", grade: .9 },
-    { name: "Gabby", grade: .9 }
-  ]; */
-
-  /* students.forEach((student, index) => {
-    let idP = $("<p>")
-      .text(index + 1)
-      .addClass("col-3 display-5")
-      .appendTo($("#display-students"));
-
-    let nameP = $("<p>")
-      .text(student.name)
-      .addClass("col-9 display-5")
-      .appendTo($("#display-students"));
-  });
-
-  var ctx = document.getElementById("myChart").getContext('2d');
-  var chart = new Chart(ctx, {
-    type: 'bar',
-
-    data: {
-      labels: students.map(student => student.name),
-      datasets: [{
-        label: "Some Data",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        borderWidth: 1,
-        data: students.map(student => student.grade)
-      }]
-    },
-
-    options: {}
-  }); */
 
 });
