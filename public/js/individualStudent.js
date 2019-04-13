365504832a3517640af13f16c367ccdd0d16266c
Dan <danmorales209@gmail.com>
2019-04-12 20:22:03 -0700

    buttons on landing page now direct user to individual student grades

--- public/js/individualStudent.js
+++ public/js/individualStudent.js
@@ -1,3 +1,63 @@
+var printGrades = function (sid) {
+  $.get("/api/students/" + sid).then(function (data) {
+
+    $("#studentNameDisplay").text(data.name);
+
+    var $tableBody = $("#studentTable");
+    $tableBody.empty();
+
+    for (var i = 0; i < data.Grades.length; i++) {
+
+      var newRow = $("<tr>");
+
+      newRow.append(`<td>${data.Grades[i].Assignment.assignmentName}</td>`);
+
+      var inputCell = $("<td>")
+        .css({
+          "display": "inline-block",
+        });
+
+      inputCell.append($("<input>")
+        .attr("id", "A-" + data.Grades[i].Assignment.id)
+        .addClass("mx-2 grade")
+        .val(data.Grades[i].gradeValue)
+      );
+
+      var editButton = $("<button>")
+        .addClass("btn btn-secondary mx-2 editButton")
+        .attr({
+          "data-sid": sid,
+          "data-aid": data.Grades[i].Assignment.id,
+        })
+        .text("Edit");
+
+      inputCell.append(editButton);
+
+      newRow.append(inputCell);
+      $tableBody.append(newRow);
+
+    }
+
+    $(".editButton").on("click", function (event) {
+
+      event.preventDefault();
+
+      var studentId = $(this).data("sid");
+      var assignmentId = $(this).data("aid");
+      var newGrade = Number($(this).siblings(".grade").val().trim());
+
+      $.ajax({
+        method: "PUT",
+        url: "/api/grades/" + studentId + "/" + assignmentId,
+        data: { gradeValue: newGrade }
+      }).then(function (data) {
+        console.log(data);
+      });
+
+    });
+  });
+}
+
 $(document).ready(function () {
 
   $.get("/api/students").then(function (data) {
@@ -19,65 +79,18 @@ $(document).ready(function () {
   });
 
 
+  if (sessionStorage.getItem("sid")) {
+
+    var studentID = sessionStorage.getItem("sid");
+    sessionStorage.removeItem("sid");
+    printGrades(studentID);
+  }
 
   $("#individualStudentChoices").on("click", " .clickme", function () {
     var studentID = $(this).data("id");
 
-    $.get("/api/students/" + studentID).then(function (data) {
+    printGrades(studentID);
 
-      $("#studentNameDisplay").text(data.name);
+  });
 
-      var $tableBody = $("#studentTable");
-      $tableBody.empty();
-
-      for (var i = 0; i < data.Grades.length; i++) {
-
-        var newRow = $("<tr>");
-
-        newRow.append(`<td>${data.Grades[i].Assignment.assignmentName}</td>`);
-
-        var inputCell = $("<td>")
-          .css({
-            "display": "inline-block",
-          });
-
-        inputCell.append($("<input>")
-          .attr("id", "A-" + data.Grades[i].Assignment.id)
-          .addClass("mx-2 grade")
-          .val(data.Grades[i].gradeValue)
-        );
-
-        var editButton = $("<button>")
-          .addClass("btn btn-secondary mx-2 editButton")
-          .attr({
-            "data-sid": studentID,
-            "data-aid": data.Grades[i].Assignment.id,
-          })
-          .text("Edit");
-
-        inputCell.append(editButton);
-
-        newRow.append(inputCell);
-        $tableBody.append(newRow);
-
-      }
-      $(".editButton").on("click", function (event) {
-
-        event.preventDefault();
-
-        var studentId = $(this).data("sid");
-        var assignmentId = $(this).data("aid");
-        var newGrade = Number($(this).siblings(".grade").val().trim());
-
-        $.ajax({
-          method: "PUT",
-          url: "/api/grades/" + studentId + "/" + assignmentId,
-          data: { gradeValue: newGrade }
-        }).then(function (data) {
-          console.log(data);
-        })
-
-      });
-    })
-  })
 });
