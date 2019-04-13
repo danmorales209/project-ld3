597e1b1a4be4b5ce05c0664c8884f4694add5344
Dan <danmorales209@gmail.com>
2019-04-12 19:02:56 -0700

    remove console.logs

--- routes/assignments-api.js
+++ routes/assignments-api.js
@@ -8,10 +8,10 @@ module.exports = function (app) {
   });
 
   app.post("/api/assignments", function (req, res) {
+
     db.Assignment.create(req.body).then(function (data) {
-      // res.json(data);
-      console.log(data);
       var newId = data.dataValues.id;
+
       db.Students.findAll().then(function (students) {
         var newAssignGrades = students.map(function (student) {
           return {
@@ -19,7 +19,7 @@ module.exports = function (app) {
             AssignmentId: newId
           };
         });
-        // console.log(studentIds);
+
         db.Grades.bulkCreate(newAssignGrades).then(function (result) {
           res.json(result);
         });
@@ -28,7 +28,7 @@ module.exports = function (app) {
   });
 
   app.put("/api/assignments/:id", function (req, res) {
-    console.log(req.body);
+    
     db.Assignment.update(
       req.body,
       {
