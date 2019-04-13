5d8221614454a739aec604e9b7022ff68e376fe1
Dan <danmorales209@gmail.com>
2019-04-12 19:01:00 -0700

    remove comments

--- routes/assignments-api.js
+++ routes/assignments-api.js
@@ -7,15 +7,6 @@ module.exports = function (app) {
     });
   });
 
-  // app.get("/api/assignments/:id", function (req, res) {
-  //   db.Assignment.findOne({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function (data) {
-  //     res.json(data);
-  //   });
-  // });
   app.post("/api/assignments", function (req, res) {
     db.Assignment.create(req.body).then(function (data) {
       // res.json(data);
@@ -35,6 +26,7 @@ module.exports = function (app) {
       });
     });
   });
+
   app.put("/api/assignments/:id", function (req, res) {
     console.log(req.body);
     db.Assignment.update(
@@ -48,17 +40,3 @@ module.exports = function (app) {
     });
   });
 };
-
-// app.get("/api/authors/:id", function(req, res) {
-//   // Here we add an "include" property to our options in our findOne query
-//   // We set the value to an array of the models we want to include in a left outer join
-//   // In this case, just db.Post
-//   db.Author.findOne({
-//     where: {
-//       id: req.params.id
-//     },
-//     include: [db.Post]
-//   }).then(function(dbAuthor) {
-//     res.json(dbAuthor);
-//   });
-// });
